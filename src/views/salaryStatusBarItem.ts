9daebda1209307a7105068047dd4f08cba76f0c7
夏坤明 <xkmwb1335@sp.noahgroup.com>
2024-05-16 18:26:34 +0800

    添加配置项；更新配置之后更新值

--- src/views/salaryStatusBarItem.ts
+++ src/views/salaryStatusBarItem.ts
@@ -1,40 +1,73 @@
 import * as vscode from "vscode";
 
-let monthlySalary = 10000; // 月薪
-let workStartTime = 9; // 上班时间，以24小时制表示
-let workEndTime = 18; // 下班时间，以24小时制表示
-let totalWorkDays = 22; // 总工作天数
+const SECONDS_PER_HOUR = 60 * 60;
 
-let dailySalary = monthlySalary / totalWorkDays; // 日薪
-let workHoursPerDay = workEndTime - workStartTime; // 每天工作小时数
-let secondsPerDay = workHoursPerDay * 60 * 60; // 每天工作秒数
-let secondSalary = dailySalary / secondsPerDay; // 秒薪
+let salaryConfig = vscode.workspace.getConfiguration("personalkit.salary");
+let monthlySalary: number = salaryConfig.get("monthlySalary") || 10000;
+let workStartTime: number = salaryConfig.get("workStartTime") || 9;
+let workEndTime: number = salaryConfig.get("workEndTime") || 18;
+let totalWorkDays: number = salaryConfig.get("totalWorkDays") || 22;
 
-let salary = 0;
+let dailySalary = monthlySalary / totalWorkDays;
+let workHoursPerDay = workEndTime - workStartTime;
+let secondsPerDay = workHoursPerDay * SECONDS_PER_HOUR;
+let secondSalary = dailySalary / secondsPerDay;
+
+let currentSalary = 0;
+
 export let salaryStatusBarItem = vscode.window.createStatusBarItem(
   vscode.StatusBarAlignment.Right,
   100
 );
 
-export function initStatusBarItem() {
+function calculateAndDisplaySalary() {
   let now = new Date();
   let currentHour = now.getHours();
   let currentMinute = now.getMinutes();
   let currentSecond = now.getSeconds();
   if (currentHour >= workStartTime && currentHour < workEndTime) {
-    let secondsWorkedToday = ((currentHour - workStartTime) * 60 * 60) + (currentMinute * 60) + currentSecond;
-    salary = secondsWorkedToday * secondSalary;
+    let secondsWorkedToday =
+      (currentHour - workStartTime) * SECONDS_PER_HOUR +
+      currentMinute * 60 +
+      currentSecond;
+    currentSalary = secondsWorkedToday * secondSalary;
+  } else if (currentHour >= workEndTime) {
+    currentSalary = secondsPerDay * secondSalary;
   } else {
-    salary = 0;
+    currentSalary = 0;
   }
-  salaryStatusBarItem.text = `今日薪资：${salary.toFixed(2)}`;
+  salaryStatusBarItem.text = `今日薪资：${currentSalary.toFixed(2)}`;
   salaryStatusBarItem.show();
+}
+
+export function initStatusBarItem() {
+  calculateAndDisplaySalary();
   setInterval(() => {
     let now = new Date();
     let currentHour = now.getHours();
     if (currentHour >= workStartTime && currentHour < workEndTime) {
-      salary += secondSalary;
-      salaryStatusBarItem.text = `今日薪资：${salary.toFixed(2)}`;
+      currentSalary += secondSalary;
+      salaryStatusBarItem.text = `今日薪资：${currentSalary.toFixed(2)}`;
     }
   }, 1000);
 }
+
+vscode.workspace.onDidChangeConfiguration((event) => {
+  if (
+    event.affectsConfiguration("personalkit.salary.monthlySalary") ||
+    event.affectsConfiguration("personalkit.salary.workStartTime") ||
+    event.affectsConfiguration("personalkit.salary.workEndTime") ||
+    event.affectsConfiguration("personalkit.salary.totalWorkDays")
+  ) {
+    salaryConfig = vscode.workspace.getConfiguration("personalkit.salary");
+    monthlySalary = salaryConfig.get("monthlySalary") || 10000;
+    workStartTime = salaryConfig.get("workStartTime") || 9;
+    workEndTime = salaryConfig.get("workEndTime") || 18;
+    totalWorkDays = salaryConfig.get("totalWorkDays") || 22;
+    dailySalary = monthlySalary / totalWorkDays;
+    workHoursPerDay = workEndTime - workStartTime;
+    secondsPerDay = workHoursPerDay * SECONDS_PER_HOUR;
+    secondSalary = dailySalary / secondsPerDay;
+    calculateAndDisplaySalary();
+  }
+});
